69d17cb6f8ec80da192de2874689583466f05ad4
Mohandev2004 <the.mohan2004@gmail.com>
2026-01-25 00:54:26 +0530

    Database Setup with Neon DB

---
+++ config/schema.tsx
@@ -0,0 +1,16 @@
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  
+  credits: integer("credits").notNull().default(5),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull()
+});
